ff880b925668edadcf6cadbca0552c2882ae91d2
TArch64 <tarasturchenko1551@gmail.com>
2026-04-11 12:10:34 +0300

    add button styles

--- app/appwindow/start/menu_view.go
+++ app/appwindow/start/menu_view.go
@@ -23,6 +23,7 @@ func NewMenuView() *MenuView {
 
 func (v *MenuView) ViewInit(parent *qt.QWidget) {
 	layout := qt.NewQVBoxLayout2()
+	layout.SetContentsMargins(0, 50, 0, 0)
 	layout.SetObjectName("start_menu")
 	parent.SetLayout(layout.QLayout)
 
@@ -44,21 +45,30 @@ func (v *MenuView) ViewInit(parent *qt.QWidget) {
 
 func (v *MenuView) renderMenuItem(item *MenuItem) *qt.QWidget {
 	button := qt.NewQPushButton3(item.Title)
-	button.OnPressed(item.OnPressed)
+	button.OnReleased(item.OnPressed)
 
 	button.SetStyleSheet(`
-		QPushButton {
-				background-color: #E0E0E0;
-				color: white;
-				border: none;
-				padding: 10px 20px;
-		}
-		QPushButton:hover {
-				background-color: #B8B8B8;
-		}
-		QPushButton:pressed {
-				background-color: #BFBFBF;
-		}`)
+    QPushButton {
+        background-color: #dddddd;
+        color: #444444;
+        font-size: 18px;
+        font-weight: bold;
+        padding: 12px 32px;
+        border: 3px solid #ffffff;
+        border-right-color: #aaaaaa;
+        border-bottom-color: #aaaaaa;
+    }
+    QPushButton:hover {
+        background-color: #eeeeee;
+        color: #000000;
+    }
+    QPushButton:pressed {
+        background-color: #d5d5d5;
+        border-top-color: #aaaaaa;
+        border-left-color: #aaaaaa;
+        border-right-color: #ffffff;
+        border-bottom-color: #ffffff;
+    }`)
 
 	return button.QWidget
 }
